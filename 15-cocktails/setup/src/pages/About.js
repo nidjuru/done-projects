0919b9afa034883a7fd6dfbca3b6718bb3aec3ea
nidjuru <eenidjo@gmail.com>
2021-04-05 00:16:10 +0200

    cocktailAPI

---
+++ 15-cocktails/setup/src/pages/About.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <section className="section about-section">
+      <h1 className="section-title ">about our passion</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus
+        deleniti impedit sapiente quam voluptatibus aliquid rem repudiandae
+        natus possimus. Ratione iure quam soluta earum blanditiis laborum, quis
+        laboriosam at error voluptates. Obcaecati earum, modi id nisi
+        accusantium corporis ut nostrum?
+      </p>
+    </section>
+  );
+};
+
+export default About;
